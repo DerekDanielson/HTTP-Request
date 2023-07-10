abb3f074bee056c5bf865a32cce51b3b74851f4d
Derek <derekdanielson84@gmail.com>
2023-07-10 09:23:58 -0600

    Notes added

---
+++ app.js
@@ -0,0 +1,6 @@
+// HyperText Transfer Protocol (HTTP) - the protocol for sending and receiving data on the web
+
+// HTTP is a "client/server" protocol. Requests are initiated by the client and the server will respond with a "response"
+
+// the response could be HTML, CSS, JS files, images and other resources as well as data formatted as JSON or XML
+
